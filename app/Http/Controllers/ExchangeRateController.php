7c4250f13fc79405dfe3faa0fc15492c5264a43e
lesleyfernandes <lesleyfernandes@gmail.com>
2020-02-16 19:23:57 +0000

    Code clean-up

--- app/Http/Controllers/ExchangeRateController.php
+++ app/Http/Controllers/ExchangeRateController.php
@@ -1,10 +1,12 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use DateTime;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use App\ExchangeRateHistory;
-use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Client;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Redirect;
@@ -12,56 +14,49 @@ use Illuminate\Support\Facades\Redirect;
 class ExchangeRateController extends Controller
 {
 
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function index()
+    public function index(): Response
     {
         return view('exchangerate.index');
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return Response
-     */
-    public function results()
+    public function results(): Response
     {
         $exchangerates = ExchangeRateHistory::orderby('created_at', 'DESC')->get();
 
         return view('exchangerate.results', compact('exchangerates'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param Request $request
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //Validates the fields birthday and currency
-        $exchangeRate = $this->validate(request(), [
-            'birthday' => 'date_format:d/m/Y|required|after:-1 year',
-            'currency' => 'required'
-        ]);
+        $exchangeRate = $this->validate(
+            request(),
+            [
+                'birthday' => 'date_format:d/m/Y|required|after:-1 year',
+                'currency' => 'required'
+            ]
+        );
 
-        //Prepares the date field to be manipulated
-        $birthday = \DateTime::createFromFormat('d/m/Y', $exchangeRate['birthday']);
+        $birthday = DateTime::createFromFormat('d/m/Y', $exchangeRate['birthday']);
+
         $exchangeRate['birthday'] = $birthday->format('Y-m-d');
 
         //Connects to the fixer.io API to get the aimed exchange rate
         //Important: GBP is not available in the free plan
         $client = new Client();
-        $response = $client->get('http://data.fixer.io/api/' . $exchangeRate['birthday'] . '?access_key=ad7394270ea88a22bda2f29ecf77d90a&base=EUR&symbols=' . $exchangeRate['currency']);
+
+        $response = $client->get(
+            'http://data.fixer.io/api/' .
+            $exchangeRate['birthday'] .
+            '?access_key=ad7394270ea88a22bda2f29ecf77d90a&base=EUR&symbols=' .
+            $exchangeRate['currency']
+        );
+
         $data = json_decode($response->getBody());
 
-        //Check the http response status and return an error if not 200 (success)
         if ($response->getStatusCode() != 200) {
-            return Redirect::back()->withErrors(['Service Unavailable, please try again later.']);
+            return Redirect::back()->withErrors(
+                ['Service Unavailable, please try again later.']
+            );
         }
 
         $exchangeRate['rate'] = $data->rates->{$exchangeRate['currency']};
@@ -88,7 +83,15 @@ class ExchangeRateController extends Controller
         $exchange_rate_record->save();
 
         //Redirects to the results page passing parameters to be used in the view.
-        return redirect()->route('results')->with(['rate' => $exchangeRate['rate'], 'birthday' => $exchangeRate['birthday'], 'currency' => $exchangeRate['currency']]);
+        return redirect()
+            ->route('results')
+            ->with(
+                [
+                    'rate' => $exchangeRate['rate'],
+                    'birthday' => $exchangeRate['birthday'],
+                    'currency' => $exchangeRate['currency']
+                ]
+            );
     }
 
 }
